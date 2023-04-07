841dac6d288592546a36cfeac6a9d32726d7b855
Aaron <aaturner1995@gmail.com>
2023-04-07 20:33:11 -0300

    update employee is now functional

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const table = require('console.table');
 const inquirer = require('inquirer');
 require('dotenv').config();
 
+const {viewDepartments, viewEmployees, viewRoles} = require('./lib/view.js');
+const {addRole, addDepartment, addEmployee} = require('./lib/add.js');
+const updateEmployeeRole = require('./lib/update.js');
+    
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -41,31 +45,31 @@ const startPrompt = () => {
         switch (answer.initialPrompt) {
             case 'View all departments':
                 // Call a function to handle viewing all departments
-                viewDepartments();
+                viewDepartments(db,startPrompt);
                 break;
             case 'View all roles':
                 // Call a function to handle viewing all roles
-                viewRoles();
+                viewRoles(db,startPrompt);
                 break;
             case 'View all employees':
                 // Call a function to handle viewing all employees
-                viewEmployees();
+                viewEmployees(db,startPrompt);
                 break;
             case 'Add a department':
                 // Call a function to handle adding a department
-                addDepartment();
+                addDepartment(db,startPrompt);
                 break;
             case 'Add a role':
                 // Call a function to handle adding a role
-                addRole();
+                addRole(db,startPrompt);
                 break;
             case 'Add an employee':
                 // Call a function to handle adding an employee
-                addEmployee();
+                addEmployee(db,startPrompt);
                 break;
             case 'Update an employee role':
                 // Call a function to handle updating an employee's role
-                updateEmployeeRole();
+                updateEmployeeRole(db,startPrompt);
                 break;
             case 'Exit':
                 // End the connection and exit the application
@@ -78,117 +82,4 @@ const startPrompt = () => {
         })
 };
 
-const viewDepartments = () => {
-    db.query(`SELECT * FROM departments`, (err,results) => {
-        console.table(results);
-        startPrompt();
-    })
-};
-
-const viewRoles = () => {
-    db.query(`SELECT * FROM roles`, (err,results) => {
-        console.table(results);
-        startPrompt();
-    })
-};
-
-const viewEmployees = () => {
-    db.query(`SELECT * FROM employee`, (err,results) => {
-        console.table(results);
-        startPrompt();
-    })
-};
-
-const addDepartment = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Please provide the name of the new department:',
-            name: 'newDepartment'
-        }
-    ]).then((answer) => {
-        db.query(`INSERT INTO departments (name) VALUES ('${answer.newDepartment}')`)
-    }).then(() => {
-        console.log('Department Added');
-        startPrompt();
-    })
-    .catch((err) => {
-        if (err.code === 'ER_DUP_ENTRY') {
-            console.log('ERROR: Department Already Exists');
-        }
-        else {
-            console.log(err);
-        }
-        startPrompt();
-    });
-};
-
-const addRole = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Please provide the title of the new role:',
-            name: 'newRoleTitle'
-        },
-        {
-            type: 'input',
-            message: 'Please provide the salary of the new role:',
-            name: 'newRoleSalary'
-        },
-        {
-            type: 'input',
-            message: 'Please provide the department ID of the new role:',
-            name: 'newRoleDeptId'
-        },
-    ]).then((answer) => {
-        db.query(`INSERT INTO roles (name) VALUES 
-        ('${answer.newDepartment}', ${answer.newRoleSalary}, ${answer.newRoleDeptId})`)
-    }).then(() => {
-        console.log('New Role Added');
-        startPrompt();
-    })
-    .catch((err) => {
-        console.log(err);
-        startPrompt();
-    });
-};
-
-const addEmployee= () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Please provide the employee first name:',
-            name: 'firstName'
-        },
-        {
-            type: 'input',
-            message: 'Please provide the employee last name:',
-            name: 'lastName'
-        },
-        {
-            type: 'input',
-            message: 'Please provide the employee role ID:',
-            name: 'roleId'
-        },
-        {
-            type: 'input',
-            message: 'Please provide the employee manager ID:',
-            name: 'managerId'
-        },
-    ]).then((answer) => {
-        db.query(`INSERT INTO roles (name) VALUES 
-        ('${answer.firstName}', '${answer.lastName}', ${answer.roleId}, ${answer.managerId})`)
-    }).then(() => {
-        console.log('New Role Added');
-        startPrompt();
-    })
-    .catch((err) => {
-        console.log(err);
-        startPrompt();
-    });
-};
-
-const updateEmployeeRole = () => {
-
-};
 
